1b96ab88bcd2a5e48ce4600ff986dc8bebad29fa
Shivam-Developer9 <63183874+Shivam-Developer9@users.noreply.github.com>
2020-06-10 19:26:36 +0530

    Intial

---
+++ constructorFunction.js
@@ -0,0 +1,19 @@
+// function Students(name, age, course, languages)
+// {
+//     this.name = name;
+//     this.age = age;
+//     this.course = course;
+//     this.languages = languages;
+//     this.study = function ()
+//     {
+//         console.log("study");
+//     }
+// }
+
+// var student1 = new Students("Shivam", 22, "CSE", ["English","Hindi","Punjabi"]);
+
+// console.log(student1.study());
+
+var ser = ["ad","bc", "sd"];
+
+if(ser.includes())
